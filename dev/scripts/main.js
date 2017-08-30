ba0b9c0ab7eb554549d483a17f93df6e197be372
chantalebarnard <barnardchantale@gmail.com>
2017-08-30 10:33:03 -0400

    unsaved changed

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -7,6 +7,7 @@ app.venueBaseUrl = 'https://api.setlist.fm/rest/1.0/artist/c649f061-c881-4cdb-98
 function handleSubmit(e) {
 	e.preventDefault();
 	let artist = $('.artist').val();
+
 };
 app.getArtis = (id) => {
 return $.ajax({
@@ -14,18 +15,21 @@ return $.ajax({
 		method: 'GET',
 		dataType: 'jsonp',
 			data: {
-				reqUrl: app.baseUrl/ + '&artistName=' + 
+				reqUrl: app.baseUrl
 			},
 	}).then(function() {
-		console.log('it works');
+		
 	});
 };
 
 app.events = () => {
 	$('form').on('submit');
+	console.log('it works');
 };
+
 app.init = function(){
 	app.events();
 };
+
 
 $(app.init);
